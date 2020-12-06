004a968cff379df5a245e9c2031bb187c79719ad
7arthy <73723377+7arthy@users.noreply.github.com>
2020-12-06 05:03:33 +0400

    Update librarian.cpp

--- librarian.cpp
+++ librarian.cpp
@@ -5,3 +5,11 @@ librarian::librarian(string x, string y) {
 	userName = x;
 	passWord = y;
 }
+
+string librarian::getName() {
+	return userName;
+}
+
+string librarian::getPass() {
+	return passWord;
+}
